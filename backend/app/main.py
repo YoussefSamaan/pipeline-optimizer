962a834baa91b1cf5b47996604cd0b5f49b70827
Youssef Samaan <ysamaan2001@gmail.com>
2025-12-21 21:49:57 -0500

    update Config, run script, formating and workflow

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,5 +19,6 @@ app.include_router(api_router, prefix="/api/v1")
 
 if __name__ == "__main__":
     import os
+
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
